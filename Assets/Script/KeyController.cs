94a62694adf487a1dffcf5e4f1c6d393a57b8f5b
DamLC <dlc65@free.fr>
2021-07-08 17:08:38 +0200

    function OnCollisionEnter()

---
+++ Assets/Script/KeyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            GameObject.Find("DoorExitPoint").GetComponent<DoorController>().CanOpen = true;
+            GetComponent<AudioSource>().Play();
+            // Destroy(this.gameObject);
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
+            Destroy(transform.parent.gameObject,2f);
+        }
+    }
+
+
+}
