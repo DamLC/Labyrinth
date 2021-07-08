fe38de1c006661ddbc267630caa236d898a1b81a
DamLC <dlc65@free.fr>
2021-07-08 17:13:10 +0200

    Function Start() and OnCollisionEnter()
    load the next level when the player enter in collision with the end teleport of the level

---
+++ Assets/Script/AfterExit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AfterExit : MonoBehaviour
+{
+    [SerializeField]
+    int LevelToLaod;
+
+    [SerializeField]
+    bool AutoIndex = true;
+
+    private void Start()
+    {
+        LevelToLaod = SceneManager.GetActiveScene().buildIndex + 1;
+             
+    }
+
+    IEnumerator LevelCompleted()
+    {
+        yield return new WaitForSeconds(2f);
+      SceneManager.LoadScene(LevelToLaod);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            GameObject.Find("Player").GetComponent<PlayerController>().LevelCompleted();
+            PlayerPrefs.SetInt("LastLevel", LevelToLaod);
+            StartCoroutine(LevelCompleted());
+            
+        }
+    }
+    
+    
+}
